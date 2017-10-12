1d53af19616a3eaedcf84654549e1d6945164b7e
Evandro Nuss <evandronuss@outlook.com>
2017-10-12 15:16:20 -0300

    .

--- app/scenes/CheckinMinistrante.js
+++ app/scenes/CheckinMinistrante.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
+import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import {
   Alert,
   Picker,
   StyleSheet,
   View
 } from 'react-native';
+import URL_API from '../lib/Configuracoes';
+import MessageDate from '../components/MessageDate';
+import { saveItem, getItem, isArray } from '../lib/Util';
 
 class CheckinMinistrante extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      language: '',
-      timeoutId: 0
+      cursoSelecionado: '',
+      oficinaSelecionada: '',
+      cursos: [],
+      oficinas: [],
+      timeoutId: 0,
+      date: ''
     };
+  }
+  
+  componentWillMount() {
+    if (this.props.isConnected) {
+      axios.get(`${URL_API}Checkin/GetCursosOficinas`)
+        .then(response => this.carregarInformacoesOnline(response))
+        .catch(() => this.carregarInformacoesOffline());
+    } else {
+      this.carregarInformacoesOffline();
+    }
   }
 
   componentWillUnmount() {
@@ -32,28 +51,88 @@ class CheckinMinistrante extends Component {
     }]);
   }
 
+  carregarInformacoesOnline(response) {
+    saveItem('CheckinDropDown', JSON.stringify(response));
+
+    this.carregarInformacoes(response);
+  }
+
+  carregarInformacoesOffline() {
+    console.log('Erro ao recuperar os dados');
+
+    getItem('CheckinDropDown').then((response) => {
+      if (response) {
+        this.carregarInformacoes(JSON.parse(response.value), response.date);
+      } else if (this.props.isConnected) {
+        Alert.alert('', 'Nenhum Registro foi encontrado!');
+        this.setState({ visible: false });
+        Actions.pop();
+      } else {
+        Alert.alert('', 'Ops! Parece que você está sem internet.');
+        this.setState({ visible: false });
+        Actions.pop();
+      }
+    });
+  }
+
+  carregarInformacoes(response, date) {
+    if (response && isArray(response.data)) {
+      const cursos = response.data;
+      const oficinas = response.data[0].oficinas;
+
+      this.setState({
+        cursoSelecionado: cursos[0].id,
+        oficinaSelecionada: oficinas[0].id,
+        cursos,
+        oficinas,
+        visible: false,
+        date
+      });
+    } else if (this.props.isConnected) {
+      Alert.alert('', 'Ops! Ocorreu um erro ao carregar os dados.');
+      this.setState({ visible: false });
+      Actions.pop();
+    } else {
+      Alert.alert('', 'Ops! Parece que você está sem internet.');
+      this.setState({ visible: false });
+      Actions.pop();
+    }
+  }
+
   topContent() {
     return (
       <View
         style={styles.container}
       >
+        <MessageDate date={this.state.date} />
         <Picker
           style={styles.picker}
           prompt="Cursos"
-          selectedValue={this.state.language}
-          onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}
+          selectedValue={this.state.cursoSelecionado}
+          onValueChange={itemValue => {
+            const curso = this.state.cursos.filter(c => c.id === itemValue);
+            const oficinas = curso[0].oficinas;
+
+            this.setState({
+              cursoSelecionado: itemValue,
+              oficinas,
+              oficinaSelecionada: oficinas[0].id,
+            });
+          }}
         >
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          {this.state.cursos.filter(c => c.nome !== '').map(c =>
+            <Picker.Item key={`Curso_${c.id}`} label={c.nome} value={c.id} />
+          )}
         </Picker>
         <Picker
           style={styles.picker}
           prompt="Oficinas"
-          selectedValue={this.state.language}
-          onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}
+          selectedValue={this.state.oficinaSelecionada}
+          onValueChange={itemValue => this.setState({ oficinaSelecionada: itemValue })}
         >
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          {this.state.oficinas.filter(o => o.nome !== '').map(o =>
+            <Picker.Item key={`Oficina_${o.id}`} label={o.nome} value={o.id} />
+          )}
         </Picker>
       </View>
     );
@@ -82,7 +161,8 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = state => (
   {
-		email: state.AutenticacaoReducer.email
+		email: state.AutenticacaoReducer.email,
+    isConnected: state.ConnectionReducer.isConnected
   }
 );
 
